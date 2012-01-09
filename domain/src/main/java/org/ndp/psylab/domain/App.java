c0a89e25d07ccdc359dcdb6cf8585666fcc0c409
Dmytro <dmitry.ndp@gmail.com>
2012-01-09 23:28:45 +0200

    Refactored project as template

---
+++ domain/src/main/java/org/ndp/psylab/domain/App.java
@@ -0,0 +1,13 @@
+package org.ndp.psylab.domain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ rest/src/main/java/org/ndp/psylab/rest/App.java
@@ -0,0 +1,13 @@
+package org.ndp.psylab.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
